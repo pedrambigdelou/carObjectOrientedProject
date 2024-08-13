5e126ba9abc28ea3e1398ecb2112f74fb919ceab
pedrambigdelou <pedrambigdelou@gmail.com>
2024-08-13 17:20:03 -0400

    Part Four: class Garage was added

--- app.js
+++ app.js
@@ -30,3 +30,20 @@ class Motorcycle extends Vehicle {
     return "VROOM!!!"
   }
 }
+
+class Garage {
+  constructor(capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+  add(newVehicle) {
+    if (!(newVehicle instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    }
+    if (this.vehicles.length >= this.capacity) {
+      return "Sorry, we're full.";
+    }
+    this.vehicles.push(newVehicle);
+    return "Vehicle added!";
+  }
+}
