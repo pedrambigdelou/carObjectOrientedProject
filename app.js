e3eec3a6aefafc01b22026937434c504301612d7
pedrambigdelou <pedrambigdelou@gmail.com>
2024-08-13 17:05:30 -0400

    class Vehicle was added

---
+++ app.js
@@ -0,0 +1,15 @@
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  honk() {
+    return "Beep.";
+  }
+
+  toString() {
+    return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+  }
+}
